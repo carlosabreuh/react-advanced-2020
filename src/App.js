9b09fcf6cb878751cb39b7f97ed4f285db3a9d4c
carlosabreuh <carlos@abreuh.com>
2021-03-16 14:00:48 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Setup from './tutorial/1-useState/setup/4-useState-object'
+import Final from './tutorial/1-useState/final/2-useState-basics'
 function App() {
   return (
     <div className='container'>
         <Setup/>
+        <Final />
     </div>
   )
 }
